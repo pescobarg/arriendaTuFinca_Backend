e5a00d40abf5fc084ed4dbba6e36395d420118f7
CryoWarrior <diegoalejandrojararojas@gmail.com>
2024-08-26 18:22:05 -0500

    Correccion en errores

--- src/main/java/com/proyecto/web/errores/ManejadorExcepciones.java
+++ src/main/java/com/proyecto/web/errores/ManejadorExcepciones.java
@@ -1,17 +1,13 @@
 package com.proyecto.web.errores;
 
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
-import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.boot.json.JsonParseException;
+
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.text.ParseException;
 import java.util.Calendar;
 
 @RestControllerAdvice
@@ -23,24 +19,21 @@ public class ManejadorExcepciones {
     public ResponseEntity<MensajeError> manejarExcepcion(ResourceNotFound e) {
         e.printStackTrace();
         MensajeError mensajeError = new MensajeError("Recurso no encontrado", Calendar.getInstance().getTime(), e.getMessage());
-        HttpHeaders responseHeaders= new HttpHeaders();
-        return new ResponseEntity<>(mensajeError,responseHeaders, HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(mensajeError, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<MensajeError> manejarExcepcion(DataIntegrityViolationException e) {
         e.printStackTrace();
         MensajeError mensajeError = new MensajeError("Un valor no repetible ya se encuentra almacenado", Calendar.getInstance().getTime(), e.getMessage());
-        HttpHeaders responseHeaders= new HttpHeaders();
-        return new ResponseEntity<>(mensajeError,responseHeaders, HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(mensajeError, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<MensajeError> manejarExcepcion(IllegalArgumentException e) {
         e.printStackTrace();
         MensajeError mensajeError = new MensajeError("Valor no valido", Calendar.getInstance().getTime(), e.getMessage());
-        HttpHeaders responseHeaders= new HttpHeaders();
-        return new ResponseEntity<>(mensajeError,responseHeaders, HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(mensajeError, HttpStatus.UNAUTHORIZED);
     }
 
 
@@ -48,15 +41,13 @@ public class ManejadorExcepciones {
     public ResponseEntity<MensajeError> manejarExcepcion(HttpMessageNotReadableException e) {
         e.printStackTrace();
         MensajeError mensajeError = new MensajeError("Error al leer datos", Calendar.getInstance().getTime(), e.getMessage());
-        HttpHeaders responseHeaders= new HttpHeaders();
-        return new ResponseEntity<>(mensajeError,responseHeaders, HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(mensajeError, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<MensajeError> manejarExcepcion(Exception e) {
         e.printStackTrace();
         MensajeError mensajeError = new MensajeError("Error en la peticion", Calendar.getInstance().getTime(), e.getMessage());
-        HttpHeaders responseHeaders= new HttpHeaders();
-        return new ResponseEntity<>(mensajeError,responseHeaders, HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(mensajeError, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/com/proyecto/web/errores/ResourceNotFound.java
+++ src/main/java/com/proyecto/web/errores/ResourceNotFound.java
@@ -1,5 +1,9 @@
 package com.proyecto.web.errores;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFound extends RuntimeException {
     public ResourceNotFound(String message) {
         super(message);
