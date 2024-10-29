a978bf116b27951b242bc28f26595eaacf3e68da
oscarRodriguez13 <149274847+oscarRodriguez13@users.noreply.github.com>
2024-10-29 07:01:10 -0500

    Update ReseniaControlador.java

--- src/main/java/com/proyecto/web/controladores/ReseniaControlador.java
+++ src/main/java/com/proyecto/web/controladores/ReseniaControlador.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://127.0.0.1")
 @RequestMapping("/api/resenias")
 public class ReseniaControlador {
 
