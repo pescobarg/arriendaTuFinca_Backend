3d7ced56fc82653a6282624c51dd0f805034df3a
oscarRodriguez13 <149274847+oscarRodriguez13@users.noreply.github.com>
2024-10-29 07:00:41 -0500

    Update AlquilerControlador.java

--- src/main/java/com/proyecto/web/controladores/AlquilerControlador.java
+++ src/main/java/com/proyecto/web/controladores/AlquilerControlador.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://127.0.0.1")
 @RestController
 @RequestMapping("/api/alquileres")
 public class AlquilerControlador {
